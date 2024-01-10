1a944a210bc2a073cce18e1d36e42384ae6d5876
Hugues Prud'homme <70539777+Huguesmmm@users.noreply.github.com>
2024-01-10 14:26:27 -0500

    add email verification on signup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -22,11 +22,13 @@ const Page = () => {
         resolver: zodResolver(AuthCredentialsValidator),
     })
 
-    const { data } = trpc.anyApiRoute.useQuery()
-    console.log(data)
+    // const { data } = trpc.anyApiRoute.useQuery()
+    // console.log(data)
+
+    const {mutate, isLoading} = trpc.auth.createPayloadUser.useMutation({})
 
     const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-        // send data to the server
+        mutate({email, password})
     }
 
     return <>
@@ -66,6 +68,7 @@ const Page = () => {
                                 <Label htmlFor="password">Password</Label>
                                 <Input
                                     {...register("password")}
+                                    type="password"
                                     className={cn({
                                         "focus-visible:ring-red-500": errors.password
                                     })}
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,14 +5,21 @@ import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
 
 import { buildConfig } from "payload/config";
+import { Users } from "./collections/Users";
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+})
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-    collections: [],
+    collections: [Users],
     routes: {
         admin: '/sell',
     },
     admin: {
+        user: "users",
         bundler: viteBundler(),
         meta: {
             titleSuffix: "- DigitalHippo",
