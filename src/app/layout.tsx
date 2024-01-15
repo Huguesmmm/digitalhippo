7e1674eeccdb5362650a4ec514ab832537f9b6b1
Hugues Prud'homme <70539777+Huguesmmm@users.noreply.github.com>
2024-01-15 17:51:22 -0500

    added banner for dev purpose

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils'
 import Navbar from '@/components/Navbar'
 import Providers from '../components/Providers'
 import { Toaster } from 'sonner'
+import Banner from '@/components/Banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className={cn("relative h-full font-sans antialiased", inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
           <Providers>
+            <Banner />
             <Navbar />
             <div className='flex-grow flex-1'>
               {children}
