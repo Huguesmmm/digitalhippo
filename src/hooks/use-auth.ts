c146bd92a5f7fd1aec9081172a736925d8f5d7c2
Hugues Prud'homme <70539777+Huguesmmm@users.noreply.github.com>
2024-01-17 12:16:52 -0500

    trying to correct router problem

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -1,28 +1,34 @@
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
+import dotenv from "dotenv"
+import path from "path"
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+})
 
 export const useAuth = () => {
 
     const router = useRouter()
-   const signOut = async () => {
+    const signOut = async () => {
         try {
-            const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`, 
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-            })
+            const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    credentials: 'include',
+                })
             if (!res.ok) throw new Error()
 
             toast.success('Logged out successfully')
-            
+
             router.push('/sign-in')
             router.refresh()
-        }catch (error) {
+        } catch (error) {
             toast.error('Couldn\'t log out. Please try again.')
         }
-     }
-     return {signOut}
+    }
+    return { signOut }
 }
